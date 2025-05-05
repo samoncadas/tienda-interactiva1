156aa37fe85131020bb19d2a6020cf2dcfa5fb7d
samoncadas <samoncadas@unal.edu.co>
2025-05-05 12:49:15 -0500

    de todo un poco

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function innit(){
-    
+function init(){
+    var card = document.getElementById("card1")
+    card = card.innerHTML=`
+    <div id="producto">
+        <h1>lista de productos...</h1>
+        <h2 id="nombre"> Productos B: </h2>
+        <input type="number" id="num">
+        <button type="button" class="btn-warning boton" onclick="agregarCarrito()" >Agregar</button>
+    </div>
+    `
+}
+
+function agregarCarrito(){
+    var carrito = document.getElementById("carrito")
+    var cantidad = document.getElementById("num").value
+    var nombre = document.getElementById("nombre")
+    carrito = carrito.innerHTML=`<h1>${cantidad}${nombre}</h1>`
 }
